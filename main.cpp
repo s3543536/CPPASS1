a2b508b3668fa17b073da80bb9b3a6692355c9ea
1wsx10 <1wsx10@gmail.com>
2017-08-11 21:49:59 +1000

    polished input validation

--- main.cpp
+++ main.cpp
@@ -6,16 +6,32 @@ int main(int argc, char **argv) {
 	boost::program_options::options_description desc("Allowed Options");
 	desc.add_options()
 		("help", "produce help message")
-		("compression", boost::program_options::value<int>(), "set compression level");
+		("compression", boost::program_options::value<int>()->default_value(10), "[int] set compression level");
 
 	//store the command into a variable map
 	boost::program_options::variables_map var_map;
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), var_map);
+	try {
+		boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), var_map);
+	} catch(boost::program_options::invalid_option_value e) {//catch various bad options
+		std::cout << "invalid command argument\n";
+		display_help(desc);
+		return EXIT_FAILURE;
+	} catch(boost::program_options::unknown_option e) {
+		std::cout << "invalid command\n";
+		display_help(desc);
+		return EXIT_FAILURE;
+	} catch(boost::program_options::invalid_command_line_syntax e) {
+		std::cout << "invalid syntax\n";
+		display_help(desc);
+		return EXIT_FAILURE;
+	}
 	boost::program_options::notify(var_map);
 
-	if(var_map.count("help")) {
-		std::cout << desc << "\n";
-		return 1;
+
+	//commands
+	if(var_map.count("help")) {//if not 0
+		display_help(desc);
+		return EXIT_SUCCESS;
 	}
 
 	if(var_map.count("compression")) {
@@ -23,4 +39,8 @@ int main(int argc, char **argv) {
 	} else {
 		std::cout << "compression level not set\n";
 	}
+}
+
+void display_help(boost::program_options::options_description desc) {
+	std::cout << desc << std::endl;
 }
--- main.h
+++ main.h
@@ -1,3 +1,5 @@
 #include <boost/program_options.hpp>
 #include <boost/tokenizer.hpp>
 
+void display_help(boost::program_options::options_description);
+
